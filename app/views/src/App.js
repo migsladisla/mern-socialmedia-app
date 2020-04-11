92b6fb1bdca61a77d465d34dd653b748341daf3d
migs <nonoyladisla@yahoo.com.ph>
2020-04-11 23:19:36 +0800

    Setup Redux and created Register view

--- app/views/src/App.js
+++ app/views/src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Landing from './components/Landing';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
+import Landing from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route exact path="/" component={Landing} />
-        </div>
-      </Router>
+      <Provider store={ store }>
+        <Router>
+          <div className="App">
+            <Route exact path="/" component={ Landing } />
+            <Route path="/register" component={ Register } />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
